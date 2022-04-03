23c0be0fbd645f6ce02b1be14b47575f2face3e5
Zach Loomas <47288744+zloomas@users.noreply.github.com>
2022-04-03 02:01:17 -0400

    many fixes to default game

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import json
-import random
+from random import choice
 
 
 class SquirdleGame:
@@ -17,29 +17,36 @@ class SquirdleGame:
         pokedex.pop("Flabébé")
 
         # reset pokemon names to all lower case for checking
-        self._pokedex = {key.lower(): [str(i) for i in val] for key, val in pokedex.items()}
+        self.pokedex = {key.lower(): [str(i) for i in val] for key, val in pokedex.items()}
 
         # set target pokemon
         if not secret_mon:
-            self._secret_mon = random.choice(sorted(self._pokedex))
-            self._secret_mon_attr = self._pokedex[self._secret_mon]
-        elif secret_mon.lower() not in self._pokedex:
+            self._secret_mon = choice(sorted(self.pokedex))
+            self._secret_mon_attr = self.pokedex[self._secret_mon]
+        elif secret_mon.lower() not in self.pokedex:
             raise ValueError(f'{secret_mon} is not a valid pokemon, check spelling')
         else:
             self._secret_mon = secret_mon.lower()
-            self._secret_mon_attr = self._pokedex[self._secret_mon]
+            self._secret_mon_attr = self.pokedex[self._secret_mon]
+
+        for ix in [0, 3, 4]:
+            self._secret_mon_attr[ix] = float(self._secret_mon_attr[ix])
 
         # initialize game tracking attributes
-        self._guess_ix = 0
-        self._win_status = False
-        self._already_guessed = dict()
+        self.guess_ix = 0
+        self.win_status = False
+        self.already_guessed = dict()
 
-    def _check_guess(self, guess_mon):
+    def check_guess(self, guess_mon):
+        self.guess_ix += 1
+
         if guess_mon == self._secret_mon:
-            self._win_status = True
+            self.win_status = True
         else:
             guess_feedback = []
-            guess_mon_attr = self._pokedex[guess_mon]
+            guess_mon_attr = self.pokedex[guess_mon]
+            for ix in [0, 3, 4]:
+                guess_mon_attr[ix] = float(guess_mon_attr[ix])
 
             # check generation
             if guess_mon_attr[0] < self._secret_mon_attr[0]:
@@ -81,39 +88,41 @@ class SquirdleGame:
             else:
                 guess_feedback.append('=')
 
-            self._already_guessed[guess_mon] = guess_feedback
+            self.already_guessed[guess_mon] = guess_feedback
 
-        self._guess_ix += 1
+            return guess_feedback
 
-    def _make_guess(self):
+    def make_guess(self):
         guess = input("What's your guess? ")
         guess_lower = guess.lower()
-        if guess_lower not in self._already_guessed:
-            if guess_lower not in self._pokedex:
+        if guess_lower not in self.already_guessed:
+            if guess_lower not in self.pokedex:
                 print(f"I don't recognize {guess.title()}, try another pokémon")
             else:
-                self._check_guess(guess)
+                self.check_guess(guess)
         else:
-            reminder = self._pokedex[guess]
+            reminder = self.pokedex[guess]
             print(f"You've already guessed {guess.title()}, here's a reminder of its stats")
             print(f"gen: {reminder[0]} | type1: {reminder[1]} | type2: {reminder[2]}")
             print(f"height: {reminder[3]} | weight: {reminder[4]}")
             print("Try another pokémon")
 
     def play(self):
-        while self._guess_ix < 9:
-            self._make_guess()
-            if not self._win_status:
+        while self.guess_ix < 9:
+            self.make_guess()
+            if not self.win_status:
                 print(" gen | type1 | type2 |   h   |   w   | Pokémon")
-                for pokemon, stats in self._already_guessed.items():
+                for pokemon, stats in self.already_guessed.items():
                     print(f" {'   |   '.join(stats)}   | {pokemon.title()}")
             else:
-                print(f'Congratulations! You guessed the secret pokémon in {self._guess_ix} turns')
+                print(f'Congratulations! You guessed the secret pokémon in {self.guess_ix} turns')
                 break
 
-        if not self._win_status:
+        if not self.win_status:
             print(f"The secret pokémon was {self._secret_mon}")
             print("Better luck next time!")
+
+        return self._secret_mon, self.guess_ix
 
 
 if __name__ == '__main__':
